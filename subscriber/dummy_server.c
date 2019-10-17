54f9e1113994113ccbb02f9931f0623b10b08a45
NaiveWang <wanhzhiwen@gmail.com>
2019-10-17 09:41:42 +0800

    dummy server opt

--- subscriber/dummy_server.c
+++ subscriber/dummy_server.c
@@ -8,19 +8,37 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+const short default_port=20777;
+char* default_host="0.0.0.0";
+const int default_delay=1000;//1000ms
+
 int main(int argc, char **argv)
 //input port as the second arg
 {
   int i, sock, conn;
   short port;
+  char* host;
+  int delay;
   float telemetry[64]; //4*64=256
   int broadcastPermission = 1;
   //load port
   //sgetchar();
-  sscanf(argv[1], "%d", &port);
-  printf("port==%d\n", port);
-  //initialize Diry Dally fake telemetries
+  if(argc == 4)
+  {
+    host = argv[1];
+    sscanf(argv[2], "%d", &port);
+    sscanf(argv[3], "%d", &delay);
+  }
+  else
+  {
+    host = default_host;
+    port = default_port;
+    delay = default_delay;
+  }
 
+  printf("host:port==%s:%d broadcast per %dms\n", host, port, delay);
+  //initialize Diry Dally fake telemetries
+  delay*=1000;
 
   struct sockaddr_in saddr, baddr;
 
@@ -28,7 +46,7 @@ int main(int argc, char **argv)
             sizeof(broadcastPermission));
 
   saddr.sin_family = AF_INET;
-  saddr.sin_addr.s_addr = inet_addr("0.0.0.0");
+  saddr.sin_addr.s_addr = inet_addr(host);
   saddr.sin_port = htons(port);
 
   sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -44,7 +62,7 @@ int main(int argc, char **argv)
     telemetry[37]=(rand()%1024)*1.f;  // current rpm is 700, yield a 70% engine
     printf("sended %f %f %f %f\n", telemetry[33], telemetry[7], telemetry[63], telemetry[37]);
     sendto(sock, telemetry, 256, 0, &saddr, sizeof(saddr));
-    usleep(100000);
+    usleep(delay);
   }
 
   return 0;
